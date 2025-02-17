e6bf42f1e86121206a133df04c46dec8973668c7
dkrizt <donkrizt@gmail.com>
2025-02-17 22:36:09 +0100

    add product controller and product routes

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ import morgan from 'morgan';
 
 /* ROUTE IMPORTS */
 import dashboardRoutes from './routes/dashboardRoutes';
-/* import productRoutes from "./routes/productRoutes";
-import userRoutes from "./routes/userRoutes";
-import expenseRoutes from "./routes/expenseRoutes"; */
+import productRoutes from './routes/productRoutes';
+// import userRoutes from "./routes/userRoutes";
+// import expenseRoutes from "./routes/expenseRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -28,10 +28,10 @@ app.get('/hello', (req, res) => {
 
 /* ROUTES */
 app.use('/dashboard', dashboardRoutes); // http://localhost:8000/dashboard
-/* app.use("/products", productRoutes); // http://localhost:8000/products
-app.use("/users", userRoutes); // http://localhost:8000/users
-app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
- */
+app.use('/products', productRoutes); // http://localhost:8000/products
+// app.use("/users", userRoutes); // http://localhost:8000/users
+// app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
+
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, '0.0.0.0', () => {
